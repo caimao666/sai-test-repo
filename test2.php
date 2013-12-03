7e19e54ea53503d6aaf79653685e590a46d0206d
蔡 春杰 <shunketsu.sai@gu3.co.jp>
2013-12-03 19:23:22 +0900

    commit my tet1.php file

---
+++ test2.php
@@ -0,0 +1,20 @@
+<?
+$str = "HANDLE_NAME = 'Maxim', ACCOUNT_ID = '', PASSWORD = 'bdsjhdasid,dsjadiojasidoja', TEST_STRING = 'You're super cool!', MODEL_CHANGE_CNT = 0";
+echo "Current String: " . $str . "<br />";
+$testArray = str_getcsv($str, ",", "'");
+for($i=0;$i<count($testArray);$i++)
+{
+$foundSign = strpos($testArray[$i], "=");
+if($foundSign == 0){
+    $testArray[$i-1] = $testArray[$i-1] .  $testArray[$i];
+    $testArray[$i] = null;
+
+}
+}
+    $testArray = array_filter($testArray);
+    foreach($testArray as $t)
+    {
+    echo $t."<br />";
+}
+
+?>
